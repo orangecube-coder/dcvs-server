87dff90ab38ef6341314a6daa3360385df98d65e
orangecube-coder <orangecube.team@ya.ru>
2023-01-25 07:54:04 +0300

    Add /controllers/user-controller.js. Edit index.js, /router/index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
-app.use('/app', router);
+app.use('/api', router);
 
 const start = async () => {
 	try {
@@ -19,7 +19,7 @@ const start = async () => {
 			useNewUrlParser: true,
 			useUnifiedTopology: true
 		});
-		app.listen(PORT, () => console.log(`Server started on PORT = ${PORT}`))
+		app.listen(PORT, () => console.log(`Server started on PORT = ${PORT}`));
 	} catch (e) {
 		console.log(e);
 	}
